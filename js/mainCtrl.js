e827b4f23ba9240a48aae4d3e1c3afe5ffbf3ec8
jessenboone <jessenboone@gmail.com>
2017-03-29 19:41:09 -0600

    cookie chat project

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,7 +1,16 @@
-angular.module('chatroom').controller('mainCtrl', function($scope, messageService){
+ angular.module('chatroom').controller('mainCtrl', function($scope, messageService){
 
   //The getMessages function will call the getMessages method on the messageService object.
   //You'll then save the result of that request to your controller's $scope as messages ($scope.messages)
+
+$scope.getMessages = function() {
+  var promise = messageService.getMessages()
+    promise.then(function(response) {
+      $scope.messages = response;
+
+    })
+  }
+
 
 
 
@@ -10,13 +19,17 @@ angular.module('chatroom').controller('mainCtrl', function($scope, messageServic
   //pass that text to the postMessage method on the messageService object which will
   //then post it to the backend.
 
+  $scope.postMessage = function(message) {
+    var promise = messageService.postMessage(message)
+      $scope.message = "";
+    }
 
 
   //uncomment this code when your getMessages function is finished
   //This goes and gets new data every second, which mimicking a chat room experience.
-  //  setInterval(function(){
-    // $scope.getMessages();
+   setInterval(function(){
+    $scope.getMessages();
 
-  // }, 1500)
+  }, 1500)
 
 })
--- js/messageService.js
+++ js/messageService.js
@@ -1,5 +1,13 @@
 angular.module('chatroom').service('messageService', function($http){
   //Here you'll need to create two methods. One called postMessage and the other called getMessages.
+  this.getMessages = function(){
+    var promise = $http.get('https://practiceapi.devmounta.in/api/chats');
+      return promise.then(function(response) {
+        if (response.status === 200) {
+          return response.data
+      }
+    })
+  }
 
   //On the lines below create a getMessages method. This method will retrieve data from the backend.
   //The url for the get request should be 'https://practiceapi.devmounta.in/api/chats'
@@ -14,6 +22,13 @@ angular.module('chatroom').service('messageService', function($http){
   //Also, remember that $http returns a promise. So return the whole $http call (return $http(...)), so you can then use .then in your controller.
 
 
+  this.postMessage = function(message){
+    var promise =  $http.post('https://practiceapi.devmounta.in/api/chats', {message: message}).then(function(response) {
+      if (response.status === 200) {
+      return(response.data)
 
+      }
+    })
+  }
 
 });
